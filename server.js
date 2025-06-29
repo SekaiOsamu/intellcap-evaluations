6d76720888e89f506c3bef1cb962aa0220deb4c6
SekaiOsamu <raniabakouch007@gmail.com>
2025-06-29 01:26:21 +0100

    cette fois c'est la bonne please

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+});
+
 // Initialize DB
 const db = new sqlite3.Database('evaluations.db');
 
