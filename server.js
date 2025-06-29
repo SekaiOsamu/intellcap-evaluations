44ba4189a74eca1fe4d81a5762fe9d58a6828a71
SekaiOsamu <raniabakouch007@gmail.com>
2025-06-29 02:00:08 +0100

    origin last

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = 3001;
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
